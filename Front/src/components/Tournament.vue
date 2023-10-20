d03012212babeec95cd371336cd50d8ca317e559
Food Tourney Deployment <deploy@foodtourney.app>
2023-10-20 02:43:15 -0700

    test

--- Front/src/components/Tournament.vue
+++ Front/src/components/Tournament.vue
@@ -146,12 +146,14 @@ export default defineComponent({
 	   // Delete the entry not clicked
             // Store clicked entry
             if (isFirst) {
+		console.log("first?", isFirst, this.option2);
                 delete this.places1[this.option2];
                 this.places2[this.option1] = JSON.parse(
                     JSON.stringify(this.places1[this.option1])
                 );
                 delete this.places1[this.option1];
             } else {
+		console.log("first?", isFirst, this.option1);
                 delete this.places1[this.option1];
                 this.places2[this.option2] = JSON.parse(
                     JSON.stringify(this.places1[this.option2])
